a27135f95a31536978930d2c9b7c1201a95556b8
AndyLS <50944069+AndyL94@users.noreply.github.com>
2023-03-09 14:10:30 +0100

    Update PHP

--- src/log.inc.php
+++ src/log.inc.php
@@ -6,18 +6,12 @@
                 $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
                 $bdd = new PDO('mysql:host=localhost;dbname=gmail', 'andy', '', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',$pdo_options));
                 
-            
-
                 $sql = "SELECT * FROM accounts WHERE login = '$email' ";
                 $result = $bdd->prepare($sql);
                 $result->execute();
                     
-                    
-
                 if(isset($_POST['mail']) && isset($_POST['psw'])){
-                    
                     $data = $result->fetchAll();
-                    
                     if(password_verify($password, $data[0]["password"]))
                     {
                         echo "<p class=\"success\">Vous êtes bien connectés !
