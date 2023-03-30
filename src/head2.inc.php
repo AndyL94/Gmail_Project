08b7cdd7e9ec167adc779c12e0c58cec5afba624
AndyLS <50944069+AndyL94@users.noreply.github.com>
2023-03-30 15:32:08 +0200

    Update

--- src/head2.inc.php
+++ src/head2.inc.php
@@ -2,7 +2,7 @@
     class User{
         public $_css = "./css/connexion3.css";
         static $_lang = ["fr","en"];
-        public $_title = "Gmail";
+        public $_title = 'Gmail';
     }
     $_new_user = new User;
 ?>
@@ -17,4 +17,8 @@
     <title><?= $_new_user -> _title ?></title>
     <link rel="stylesheet" href=<?= $_new_user -> _css ?>>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
 </head>
